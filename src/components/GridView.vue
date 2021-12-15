81dabe86647f6b7abc729c45f469a543f704d1d8
Rogerio Bene <rogeriobene@unipampa.edu.br>
2021-12-15 15:00:51 -0300

    Grade de visualização com datatable

---
+++ src/components/GridView.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-data-table
+    :headers="cabecalho"
+    :items="meses"
+    :items-per-page="5"
+    class="elevation-1"
+    disable-sort
+  ></v-data-table>
+</template>
+
+<script>
+
+export default {
+
+    props:
+        ['meses']
+    ,
+    data(){
+        return{
+            cabecalho: [
+                {
+                    text: 'Período',
+                    align: 'start',
+                    sortable: false,
+                    value: 'anoMes' },
+                { text: 'Juro mensal', value: 'jurosAoMes' },
+                { text: 'Juros totais', value: 'saldoSomenteTaxa' },
+                { text: 'Saldo aportes', value: 'saldoSemTaxa' },
+                { text: 'Saldo total', value: 'saldo' },
+                { text: 'Diferença', value: 'diferenca' },
+                ],
+            itens: this.meses,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
