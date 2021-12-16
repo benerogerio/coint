4f15e2fc9f50631deec31f4c1a07651e286ea08e
Rogerio Bene <rogeriobene@unipampa.edu.br>
2021-12-16 15:12:42 -0300

    gráficos parcial

---
+++ src/components/Graphs.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <GraphView :itens="jurosAoMes" />
+        <GraphView :itens="saldoSomenteTaxa" />
+        <GraphView :itens="saldoSemTaxa" />
+        <GraphView :itens="saldo" />
+        <GraphView :itens="diferenca" />
+    </div>
+</template>
+
+<script>
+
+import GraphView from "./GraphView.vue";
+
+export default {
+
+    components: { GraphView },
+    data: () => ({
+        itens: []
+    }),
+    props: ['meses'],
+    methods: {
+
+    },
+    computed: {
+        jurosAoMes(){
+
+            this.meses.array.forEach(periodo => {
+                item[] = {'periodo': periodo,'valor': }
+            });
+            return item
+        },
+        saldoSomenteTaxa(){
+
+        },
+        saldoSemTaxa(){
+
+        },saldo(){
+
+        },diferenca(){
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
