fc745aabbdc618f5ab53b75880613efb45419df2
Rogerio Bene <rogeriobene@unipampa.edu.br>
2021-12-21 16:18:10 -0300

    grafico com principais series

--- src/components/Graphs.vue
+++ src/components/Graphs.vue
@@ -1,58 +1,103 @@
 <template>
-    <div>
-        <GraphView
-            :nome="'Juro mensal'"
-            :itens="calculaInfo('jurosAoMes')" />
-
-        <GraphView
-            :nome="'Juros totais'"
-            :itens="calculaInfo('saldoSomenteTaxa')" />
-
-        <GraphView
-            :nome="'Saldo aportes'"
-            :itens="calculaInfo('saldoSemTaxa')" />
-
-        <GraphView
-            :nome="'Saldo total'"
-            :itens="calculaInfo('saldo')" />
-    </div>
+    <v-card
+        class="mt-4 mx-auto"
+        max-width="500"
+    >
+        <div v-if="options && series">
+            <apexchart type="line" :options="options" :series="series"></apexchart>
+        </div>
+        <div v-else>
+            Dados inexistentes
+        </div>
+    </v-card>
 </template>
 
 <script>
 
-import GraphView from "./GraphView.vue";
+import apexchart  from "vue-apexcharts"
 
 export default {
 
-    components: { GraphView },
+    components: { apexchart },
     props: ['meses'],
-    methods: {
-        calculaInfo(info){
-
-            let qtdItens = this.meses.length
-            let maxItens = 10
-            let offset   = qtdItens / maxItens
-
-            let itens = new Object
-            itens.labels = new Array
-            itens.value = new Array
-
-            let j = 0
-            for (let i = 0; i < maxItens; i++) {
-                j = Math.round(i * offset)
-
-                itens.labels [i] = this.meses[j].anoMes
-                itens.value [i] = this.meses[j][info]
-            }
-
-            return itens
-        }
-    },
     computed: {
+        options(){
+            return {
+                chart: {
+                    id: 'vuechart-totais',
+                    width: "100%",
+                    height: "100%",
+                },
+                xaxis: {
+                    categories: this.categorias()
+                },
+                responsive: [
+                    {
+                        breakpoint: 1000,
+                        options: {
+                            plotOptions: {
+                                bar: {
+                                    horizontal: false
+                                }
+                            },
+                            legend: {
+                                position: "bottom"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        series(){
+            return [this.jurosAoMes(), this.saldoSomenteTaxa(), this.saldoSemTaxa(), this.saldo()]
+        },
         console(){
             return{console}
         },
         window: () => window
+    },
+    methods: {
+        calculaInfo(type='cat', info=''){
+
+            // Quantidade máxima de ítens exibidos no gráfico
+            let maxItens = 14
+            let qtdItens = this.meses.length
+
+            // define ofset para iteração proporcional dentro da quantidade máxima de ítens
+            let offset   = (qtdItens <= maxItens) ? 1 : qtdItens/maxItens
+
+            // trata quando a quantidade de ítens for menor que quantidade máxima
+            maxItens = (qtdItens < maxItens) ? qtdItens : maxItens
+
+            let j = 0
+            let itens = new Array
+            for (let i = 0; i < maxItens; i++) {
+
+                // itera proporcionalmente a quantidade máxima de itens
+                // trata exibição do último ítem
+                j = (i == maxItens - 1) ? qtdItens - 1 : Math.round(i * offset)
+
+                // adiciona ao array a categorias ou a informação
+                itens [i] = (type == 'cat') ? this.meses[j].anoMes : this.meses[j][info]
+            }
+
+            return itens
+        },
+        categorias(){
+            return this.calculaInfo("cat")
+        },
+        jurosAoMes(){
+            return {'name':'Juro mensal', 'data': this.calculaInfo('serie','jurosAoMes')}
+        },
+        saldoSomenteTaxa(){
+            return {'name':'Juros totais', 'data': this.calculaInfo('serie','saldoSomenteTaxa')}
+        },
+        saldoSemTaxa(){
+            return {'name':'Saldo aportes', 'data': this.calculaInfo('serie','saldoSemTaxa')}
+        },
+        saldo(){
+            return {'name':'Saldo total', 'data': this.calculaInfo('serie','saldo')}
+        },
     }
 
 }
