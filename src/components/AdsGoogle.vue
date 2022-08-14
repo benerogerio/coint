5538e054c84d26b83c13231430e32e4cd5aff24e
Rogerio Bene <rogeriobene@unipampa.edu.br>
2022-08-13 22:54:46 -0300

    nova chave admob icones cordova config.xml

--- src/components/AdsGoogle.vue
+++ src/components/AdsGoogle.vue
@@ -14,8 +14,9 @@
     methods : {
       fireBanner() {
         admob.setOptions({
-          publisherId:           "ca-app-pub-6515701583756658/1084097670",  // Required
-          //publisherId:           "ca-app-pub-3940256099942544/6300978111",  // Required Teste
+          //publisherId:           "ca-app-pub-6515701583756658~1811354396",  // Required id aplicativo
+          publisherId:           "ca-app-pub-6515701583756658/7648066639",  // Required banner id
+          //publisherId:           "ca-app-pub-3940256099942544/6300978111",  // Required Chave de Teste
           //interstitialAdId:      "ca-app-pub-3940256099942544/1033173712",  // Optional
           autoShowBanner:        true,                                      // Optional
           //autoShowRInterstitial: false,                                     // Optional
