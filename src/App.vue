60cbcd9d74dd7df787569fb64699f1727e8001f1
Rogerio Bene <rogeriobene@unipampa.edu.br>
2023-03-10 00:20:55 -0300

    Mudança na animação de transição Update para gradle 8 e geração de nova build

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@
       <v-container fluid>
 
         <keep-alive>
-            <Transition name="slide-fade" mode="out-in">
+            <v-fade-transition mode="out-in">
               <component :meses="meses" @do-calculo="doCalculo($event)" :is="componente" />
-            </Transition>
+            </v-fade-transition>
         </keep-alive>
 
         <!-- If using vue-router -->
@@ -198,17 +198,5 @@ export default {
 </script>
 
 <style>
-.slide-fade-enter-active {
-  transition: all 0.2s ease;
-}
 
-.slide-fade-leave-active {
-  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
 </style>
