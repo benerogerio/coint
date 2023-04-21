c189c7dcf87e0381bf52e50875db627cef56a774
Rogerio Bene <rogeriobene@unipampa.edu.br>
2023-04-21 20:47:01 -0300

    admob-plus-cordova

--- src/components/AdsGoogle.vue
+++ src/components/AdsGoogle.vue
@@ -1,35 +1,31 @@
 <template>
-    <div></div>
+	<div></div>
 </template>
 
 <script>
 
-  import admob from 'cordova-admob/www/admob'
+export default  {
+	data(){
+		return{
+			banner: null
+		}
+	},
+	mounted(){
+		if(window.cordova) // se cordova definido dispara banner
+			return this.fireBanner()
+	},
+	methods : {
+		async fireBanner() {
+			this.banner = new window.admob.BannerAd({
+				adUnitId: 'ca-app-pub-6515701583756658/7648066639',
+			})
 
-  export default  {
-    created(){
-      //if(Window.cordova) // se cordova definido dispara banner
-        return this.fireBanner()
-    },
-    methods : {
-      fireBanner() {
-        admob.setOptions({
-          //publisherId:           "ca-app-pub-6515701583756658~1811354396",  // Required id aplicativo
-          publisherId:           "ca-app-pub-6515701583756658/7648066639",  // Required Banner produção
-          // publisherId:           "ca-app-pub-3940256099942544/6300978111",  // Required Chave de Teste
-          //interstitialAdId:      "ca-app-pub-3940256099942544/1033173712",  // Optional
-          autoShowBanner:        true,                                      // Optional
-          //autoShowRInterstitial: false,                                     // Optional
-          //autoShowRewarded:      false,                                     // Optional
-          //tappxIdiOS:            "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
-          //tappxIdAndroid:        "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
-          //tappxShare:            0.5                                        // Optional
-        });
+			this.banner.on('impression', async () => {
+				// await this.banner.hide()
+			})
 
-        admob.createBannerView();
-
-      }
-    }
-
-  }
+			await this.banner.show()
+		}
+	}
+}
 </script>
